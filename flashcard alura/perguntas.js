d7752c4b9a019151662b8828575c254645bb4587
helo287 <00001091600673sp@al.educacao.sp.gov.br>
2024-12-03 15:24:09 -0300

    Add files via upload

---
+++ flashcard alura/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Conhecimentos Gerais',
+    'De quem é a pergunta "Penso, logo existo"?',
+    'A frase é de René Descartes'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o primeiro presidente do Brasil?',
+    'O primeiro presidente do Brasil foi Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Quimica',
+    'Quais os estados da água?',
+    'Os estados da água são líquido, sólido e gasoso'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom dia em inglês?',
+    'Bom dia em ingles é GOOD MORNING (GUD MORNING)'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'O número po tem infinitas casas decimais'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que a palavra legend significa em português?',
+    'Significa Lenda em português'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Tchau em inglês?',
+    'Tchau em ingles é BYE (BAI)'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Machu Picchu está no Peru'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz de 9?',
+    'A raiz quadrada de 9 é 3'
+)
